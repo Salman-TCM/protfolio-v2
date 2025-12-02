b7ac5bf7fd3dc547cd0191b4a6cbd14610f806fd
Salman-TCM <salman.connects@gmail.com>
2025-12-02 10:37:38 +0600

    update projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { TerminalInterface } from "@/components/terminal-interface"
 import { StatusBar } from "@/components/status-bar"
+// import { ProjectsSection } from "@/components/projects-section"
 
 export default function Home() {
   return (
     <main className="bg-black">
       <TerminalInterface />
       <StatusBar />
+      {/* <ProjectsSection /> */}
     </main>
   )
 }
